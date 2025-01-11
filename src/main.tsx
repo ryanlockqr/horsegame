d6ad164d46c10998e519d02a4aba8ec824997d75
ryanlockqr <zcabrql@ucl.ac.uk>
2025-01-11 14:58:58 +0000

    works

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Amplify } from 'aws-amplify';
 import App from './App.jsx';
-import outputs from '../amplify_outputs.json';
 import './index.css';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(outputs);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+declare module '*.jsx' {
+    const content: any;
+    export default content;
+  }
+  
