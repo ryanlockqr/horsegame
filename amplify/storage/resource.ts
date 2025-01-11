953803c1e5556399d31a7e7ce516da408fdadd07
ryanlockqr <zcabrql@ucl.ac.uk>
2025-01-11 01:36:37 +0000

    s3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,5 @@
-import { defineStorage } from "@aws-amplify/backend";
+import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: "amplifyNotesDrive",
-  access: (allow) => ({
-    "media/{entity_id}/*": [
-      allow.entity("identity").to(["read", "write", "delete"]),
-    ],
-  }),
+  name: 'horseSkins'
 });
