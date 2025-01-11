7e76e8ad5cf04a59e0babe27a5cd5aec148f94c6
ryanlockqr <zcabrql@ucl.ac.uk>
2025-01-11 13:48:30 +0000

    hors3352

--- src/horseImagePage.tsx
+++ src/horseImagePage.tsx
@@ -1,11 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import { StorageImage } from '@aws-amplify/ui-react-storage';
 
 const HorseImagePage = () => {
+  const navigate = useNavigate();  // Initialize the navigate function
 
+  const handleNavigate = () => {
+    navigate('/other');  // Navigate to the '/other' route when the button is clicked
+  };
 
   return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Horse Image</h1>
       <StorageImage alt="horse" path="skins/horse.jpg" style={{ maxWidth: '100%', height: 'auto' }} 
       />
+      <div>
+        <button onClick={handleNavigate}>Go to the Next Page</button>
+      </div>
+    </div>
   );
 };
 
